e94c4f697a3e701c6854ab3bc1e2e7cc1ef9c3bc
psbsj2016 <psbsj.2016@gmail.com>
2026-02-20 17:06:10 -0300

    Força atualizacao nos celulares (Cache Busting)

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ const io = new Server(server);
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+// --- CONFIGURAÇÃO PARA FORÇAR ATUALIZAÇÃO NO CELULAR ---
+app.use(express.static('public', {
+    etag: false,
+    setHeaders: (res, path) => {
+        res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        res.setHeader('Pragma', 'no-cache');
+        res.setHeader('Expires', '0');
+    }
+}));
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
